2dc71a6f104b91b52eb35e67562a36f3c6623622
edsonmaya <edson.maya@iest.edu.mx>
2024-10-11 22:41:13 -0600

    Initial Commit

---
+++ TareaBotones/TareaBotonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TareaBotonesApp.swift
+//  TareaBotones
+//
+//  Created by Edson Maya Méndez on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TareaBotonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TareaBotonesTests/TareaBotonesTests.swift
@@ -0,0 +1,16 @@
+//
+//  TareaBotonesTests.swift
+//  TareaBotonesTests
+//
+//  Created by Edson Maya Méndez on 11/10/24.
+//
+
+import Testing
+
+struct TareaBotonesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
